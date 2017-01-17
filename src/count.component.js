b883f8dd22910a72deb872b3e8e78a7b96537b2c
Andrew Morgan <andrew.morgan@mongodb.com>
2017-01-17 14:57:23 +0000

    added repeating reads

--- src/count.component.js
+++ src/count.component.js
@@ -7,17 +7,45 @@ export class CountDocuments extends React.Component {
     super(props);
     this.state = {
       documentCount: null,
+      repeat: false,
       errorText: null
     }
 
     this.handleCountSubmit=this.handleCountSubmit.bind(this);
+    this.handleRepeatToggle=this.handleRepeatToggle.bind(this);
+    this.countOnce=this.countOnce.bind(this);
   }
 
   componentDidMount() {
   }
 
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  handleRepeatToggle(event) {
+    this.setState({repeat: event.target.checked});
+    if (!event.target.checked) {
+      clearInterval(this.timerID);
+      this.setState({documentCount: this.state.documentCount.toLocaleString() + " document – Stopped counting"});
+    }
+  }
+
+  countOnce() {
+    let _this = this;
+    this.props.dataService.sendCountDocs(this.props.collection)
+    .then (
+      function(results) {
+        _this.setState({documentCount: results});
+      },
+      function(err) {
+        _this.setState({errorText: err});
+      })
+  }
+
   handleCountSubmit(event) {
     let _this = this;
+    clearInterval(this.timerID);
     this.setState({
       documentCount: null,
       erorText: null});
@@ -28,9 +56,14 @@ export class CountDocuments extends React.Component {
         _this.setState({documentCount: results});
       },
       function(err) {
+        clearInterval(_this.timerID);
         _this.setState({errorText: err});
       })
     
+    this.timerID = setInterval(
+      () => this.countOnce(),
+      5000
+    );
   }
 
   render() {
@@ -38,6 +71,14 @@ export class CountDocuments extends React.Component {
       <div>
         <h2>Count Documents from {this.props.collection}</h2>
         <div>
+          <label>
+            Repeat count every 5 seconds?  
+            <input type="checkbox"
+              checked={this.state.repeat}
+              onChange={this.handleRepeatToggle}
+            />
+          </label>
+          <br/><br/>
           <button onClick={this.handleCountSubmit}>
             {"Count docs in " + this.props.collection}
           </button>
